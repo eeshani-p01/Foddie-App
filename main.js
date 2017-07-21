3001a56ab2afe86994289b3157a6ea64b351d548
Eeshani Patel <eeshani.p01@gmail.com>
2017-07-21 13:58:57 +0530

    All files added till validation

---
+++ main.js
@@ -0,0 +1,15 @@
+// //run when Html file is loaded
+//
+// //window .onload in jquery
+// $(document).ready(function() {
+//   //input event on input tag ,which runs whenever we write in the input tag
+//       //it also works on backspace , keypress wont work
+//     $('#user-email').on('input',function(){   //'keyup' event
+//         var email = $('#user-email').val();
+//         //email=email.split('@')[0];  get string till @
+//         var message= 'Welcome, '+email;
+//         $('.welcome-message').text(message);
+//
+//         // console.log($('#user-email').val())
+//     });
+// });
