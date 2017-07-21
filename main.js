f499c5c455e826f846f304f49ae96d5a70a2f05f
Eeshani Patel <eeshani.p01@gmail.com>
2017-07-22 00:52:15 +0530

    Angular 4

--- main.js
+++ main.js
@@ -29,6 +29,95 @@ foodieApp.controller('homeController',function($scope){
   //$scope is our model which stores data and handle it
   //WE can add functions and properties to this object and can use them in  our html within scope
   // $scope.number =42;       {{number}}      number is our model object
-  $scope.restaurants = ['Farzi Cafe','McDonalds','Pizza Hut','Cafe Coffee Day'];
-    
+  $scope.restaurants = [
+    {
+        name:'',
+        address:'',
+        location:'',
+        category:'',
+        vote:'',
+        cuisines:'',
+        cost:'',
+        hours:'',
+        image:''
+    },
+    {
+        name:'Farzi Cafe',
+        address:'38/42, Level 2, Block C, Inner Circle, Connought Place',
+        location:'Connought Place',
+        category:'Casual Dining, Bar',
+        vote:'4.2',
+        cuisines:'Modern Indian',
+        cost:'2200',
+        hours:'12 Noon to 12 AM (Mon-Sun)',
+        image:'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+    },
+    {
+      name:'',
+      address:'',
+      location:'',
+      category:'',
+      vote:'',
+      cuisines:'',
+      cost:'',
+      hours:'',
+      image:''
+    },
+    {
+      name:'',
+      address:'',
+      location:'',
+      category:'',
+      vote:'',
+      cuisines:'',
+      cost:'',
+      hours:'',
+      image:''
+    },
+    {
+      name:'',
+      address:'',
+      location:'',
+      category:'',
+      vote:'',
+      cuisines:'',
+      cost:'',
+      hours:'',
+      image:''
+    },
+    {
+      name:'',
+      address:'',
+      location:'',
+      category:'',
+      vote:'',
+      cuisines:'',
+      cost:'',
+      hours:'',
+      image:''
+    },
+    {
+      name:'',
+      address:'',
+      location:'',
+      category:'',
+      vote:'',
+      cuisines:'',
+      cost:'',
+      hours:'',
+      image:''
+    },
+    {
+      name:'',
+      address:'',
+      location:'',
+      category:'',
+      vote:'',
+      cuisines:'',
+      cost:'',
+      hours:'',
+      image:''
+    }
+  ];
+
 })
